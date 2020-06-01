1f23634c6ea3f37cd0acf8d640dcce2e7c816e38
arqhive <hgystation@gmail.com>
2020-06-01 22:43:49 +0900

    Xcode 소스 컨트롤 연습

---
+++ proj1/main.m
@@ -0,0 +1,23 @@
+// 첫 번째 예제프로그램
+
+// 컴파일러에게 Foundation.h를 찾아 처리하도록 함
+// #import는 이 파일에 잇는 정보를 마치 프로그램의 그 위치에 그대로 입력된 것처럼 가져오거나 포함시리카는 의미이다.
+// Foundation.h에는 프로그램에서 사용할 다른 클래스와 함수에 대한 정보가 들어있다.
+#import <Foundation/Foundation.h>
+
+// main은 프로그램 실행시 시작되는 위치를 나타내는 특별한 이름이다.
+// int는 반환값이 정수임을 나타낸다.
+int main (int argc, const char * argv[])
+{
+    // 여는 중괄호와 닫는 중괄호 사이에 나타나는 프로그램 명령문슨 모두 오토릴리스 풀이라는 컨텍스느 내에서 실행되어야 한다.
+    // 오토릴리스 풀은 애플리케이션이 객체를 생성하며 사용하는 메모리를 시스템이 효과적으로 관리하도록 하는 기법이다.
+    @autoreleasepool {
+        // @"Programming is fun!" 은 NSString 객체이다.
+        // @가 없으면 C스타일의 스트링 상수를 작성하는 것이고 @을 쓰면 NSString 객체를 작성하는 것이다.
+        // NSLog는 간단히 인수를 표시하거나 로깅하는 Objective-C 라이브러리 함수다.
+        NSLog(@"Programming is fun!");
+        NSLog(@"Programming in Objective-C is even more fun!");
+    }
+    // 프로그램이 정상 종료되었다는 의미.
+    return 0;
+}
